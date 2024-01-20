ea21c4c2a8335ee18062350d297c3629a04ecce2
KiyoshiKazuma <makiyoshikazu834@gmail.com>
2024-01-20 12:08:49 +0900

    implement list SUCCESS compile

--- flex_layer_network/list.c
+++ flex_layer_network/list.c
@@ -38,38 +38,108 @@ void *get_list(H_LIST hList, unsigned int num)
         return NULL;
     }
     S_LIST *pList = (S_LIST *)pList;
-    S_LIST_ELEM * pListElem=NULL;
+    S_LIST_ELEM *pListElem = NULL;
 
     // SIZE check
     if (num == 0 || num > pList->length)
     {
         return NULL;
     }
-    pListElem=pList->pFirst;
-    for(unsigned int cnt=1;cnt<num;cnt++){
-        pListElem=pListElem->pNextList;
+    pListElem = pList->pFirst;
+    for (unsigned int cnt = 1; cnt < num; cnt++)
+    {
+        pListElem = pListElem->pNextList;
     }
     return pListElem->pElem;
 }
 
 unsigned int find_list(H_LIST hList, void *pElem)
 {
-    //NULL check
-    if(hList==NULL){
+    // NULL check
+    if (hList == NULL)
+    {
         return 0;
     }
 
-    S_LIST * pList =(S_LIST *)hList;
+    S_LIST *pList = (S_LIST *)hList;
+    S_LIST_ELEM *pListElemCurrent = NULL;
+    S_LIST_ELEM *pListElemNext = NULL;
 
-    unsigned int num=0;
-    for(unsigned)
-    
+    pListElemNext = pList->pFirst;
+
+    unsigned int num = 0;
+    for (unsigned int i = 1; i <= pList->length; i++)
+    {
+        pListElemCurrent = pListElemNext;
+        if (pListElemCurrent->pElem == pElem)
+        {
+            num = i;
+            break;
+        }
+        pListElemNext = pListElemCurrent->pNextList;
+    }
+
+    return num;
 }
 int append_list(H_LIST hList, void *pElem)
 {
-    ;
+    // NULL CHECK
+    if (hList == NULL || pElem == NULL)
+    {
+        return 1;
+    }
+
+    S_LIST *pList = (S_LIST *)hList;
+    S_LIST_ELEM *pListElemLast = NULL;
+    S_LIST_ELEM *pListElemNew = NULL;
+
+    pListElemNew = (S_LIST_ELEM *)malloc(sizeof(S_LIST_ELEM));
+    pListElemNew->pElem = pElem;
+    pListElemNew->pNextList = NULL;
+
+    if (pList->length > 0)
+    {
+        pListElemLast = get_list(hList, pList->length);
+        if (pListElemLast == NULL)
+        {
+            free(pListElemNew);
+            return 1;
+        }
+        pListElemLast->pNextList = pListElemNew;
+    }
+    else
+    {
+        pList->pFirst=pListElemNew;
+    }
+    pList->length++;
+
+    return 0;
 }
 int remove_list(H_LIST hList, unsigned int num)
 {
-    ;
+    //NULL CHECK
+    if(hList==NULL){
+        return 1;
+    }
+
+    S_LIST * pList = (S_LIST *)hList;
+
+    //size check
+    if(num>pList->length || num==0){
+        return 1;
+    }
+
+    S_LIST_ELEM * pListElemTarget =NULL;
+    S_LIST_ELEM * pListElemPre =NULL;
+
+    pListElemTarget=get_list(hList,num);
+    if(num==1){
+        pList->pFirst=pListElemTarget->pNextList;
+    }else{
+        pListElemPre=get_list(hList,num-1);
+        pListElemPre->pNextList=pListElemTarget->pNextList;
+    }
+    free(pListElemTarget);
+    pList->length--;
+    return 0;
 }
